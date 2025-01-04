5bd13db96d14422f617a066de9c4f874f62d0211
shaniquerose123 <shaniquerose.studies123@gmail.com>
2025-01-04 04:54:43 +0000

    Install Apache Airflow

--- airflow/webserver_config.py
+++ airflow/webserver_config.py
@@ -32,7 +32,7 @@ from flask_appbuilder.const import AUTH_DB
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Flask-WTF flag for CSRF
-WTF_CSRF_ENABLED = False
+WTF_CSRF_ENABLED = false
 WTF_CSRF_TIME_LIMIT = None
 
 # ----------------------------------------------------
